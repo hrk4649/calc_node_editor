4f0392060d97c71a1a8fc3d8a9ddc389c59e132f
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-07-23 08:19:16 +0900

    fix func_node

--- godot3/func_node.gd
+++ godot3/func_node.gd
@@ -2,24 +2,26 @@ extends GraphNode
 
 signal change_func(id, func_name)
 
-@onready var optionButton = $OptionButton
+onready var optionButton = $OptionButton
 
 var funcs = ["","round","floor","ceil","sin"]
 
-var node_type: 
-	get:
-		return Constraints.FUNC
-	set(_value):
-		pass
+var node_type setget set_node_type, get_node_type
+
+func get_node_type():
+    return Constraints.FUNC
+
+func set_node_type(value):
+    pass
 
 func set_func_name(func_name):
-	optionButton.select(funcs.find(func_name))
+    optionButton.select(funcs.find(func_name))
 
 func _ready():
-	for idx in range(0, funcs.size()):
-		var func_name = funcs[idx]
-		optionButton.add_item(func_name, idx)
+    for idx in range(0, funcs.size()):
+        var func_name = funcs[idx]
+        optionButton.add_item(func_name, idx)
 
 func _on_option_button_item_selected(index):
-	var func_name = funcs[index]
-	emit_signal("change_func", get_name(), func_name)
+    var func_name = funcs[index]
+    emit_signal("change_func", get_name(), func_name)
