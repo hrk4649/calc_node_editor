3f98f7548ae2d068c4de6e2ec58308551489a5b1
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-07-23 07:59:32 +0900

    fix n_ary_op_node

--- godot3/n_ary_op_node.gd
+++ godot3/n_ary_op_node.gd
@@ -1,9 +1,11 @@
 extends GraphNode
 
-var node_type: 
-	get:
-		return node_type
-	set(value):
-		node_type = value
+var node_type setget set_node_type, get_node_type
+
+func get_node_type():
+    return self.node_type
+
+func set_node_type(value):
+    self.node_type = value
 
 
