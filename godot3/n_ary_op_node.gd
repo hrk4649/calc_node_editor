25908b692b1eba21f903e3abea25a25036c412ef
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-07-23 10:40:27 +0900

    fix setter and getter

--- godot3/n_ary_op_node.gd
+++ godot3/n_ary_op_node.gd
@@ -3,9 +3,9 @@ extends GraphNode
 var node_type setget set_node_type, get_node_type
 
 func get_node_type():
-    return self.node_type
+    return node_type
 
 func set_node_type(value):
-    self.node_type = value
+    node_type = value
 
 
