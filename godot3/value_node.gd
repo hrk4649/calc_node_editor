ee58f838c60902f0c1e6ed84cd65d2fbacb89678
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-07-31 09:46:48 +0900

    resizable value_node

--- godot3/value_node.gd
+++ godot3/value_node.gd
@@ -24,12 +24,10 @@ func set_node_name(value):
     lineEditName.text = value
 
 func _on_resize_request(new_minsize):
-    self.size = new_minsize
+    self.rect_size = new_minsize
 
 func _on_line_edit_name_text_changed(new_text):
     emit_signal("change_name", self.name, new_text)
 
 func _on_line_edit_value_text_changed(new_text):
     emit_signal("change_value", self.name, new_text)
-
-
